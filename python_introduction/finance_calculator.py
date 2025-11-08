a7ba55412be2e0cb064d055a3ca72060f1c5fc0d
Nada Ayman <nada.ayman.ali224@gmail.com>
2025-11-08 12:53:53 +0200

    Adding financial calculator3

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,11 +1,11 @@
-monthely_income = int( input("Enter your monthly income: "))
-monthely_expenses = int(input("Enter your total monthly expenses: "))
+monthly_income = int( input("Enter your monthly income: "))
+monthly_expenses = int(input("Enter your total monthly expenses: "))
 
-monthely_savings = monthely_income - monthely_expenses
-str_savings = str(monthely_savings)
+monthly_savings = monthly_income - monthly_expenses
+str_savings = str(monthly_savings)
 print("Your monthly savings are", "$" + str_savings + ".")
 
-Projected_Savings =  int(monthely_savings * 12 + ( monthely_savings * 12 * 0.05))
+Projected_Savings =  int(monthly_savings * 12 + ( monthly_savings * 12 * 0.05))
 str_pro_savings = str(Projected_Savings)
 print("Projected savings after one year, with interest, is:", "$" + str_pro_savings + ".")
 
